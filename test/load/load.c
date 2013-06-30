92907f803a0d95db28a35a7811109cc51eec931b
Franco Fichtner <franco@lastsummer.de>
2013-06-30 12:04:23 +0200

    load: deduplicate unittest code, no functional changes

--- test/load/load.c
+++ test/load/load.c
@@ -28,7 +28,7 @@
 
 output_init();
 
-static const unsigned int test_pcap_len[] = {
+static const unsigned int pcap_len[] = {
 	76, 142, 62, 62, 54, 235, 63, 60, 191, 66, 72,
 	84, 72, 72, 84, 90, 62, 93, 68, 60, 110, 1514,
 	1514, 1514, 1514, 590, 1506, 590, 590, 590,
@@ -37,79 +37,41 @@ static const unsigned int test_pcap_len[] = {
 	60, 60, 82, 54, 60, 54, 102, 60, 54, 60, 243,
 };
 
-static void
-test_pcap(void)
-{
-	struct peak_load *pcap = peak_load_init("../../sample/test.pcap");
-	struct ether_header *eth;
-	unsigned int i;
+static const char *pcap_file = "../../sample/test.pcap";
 
-	assert(pcap);
-
-	eth = (struct ether_header *) pcap->buf;
-
-	for (i = 0; i < lengthof(test_pcap_len); ++i) {
-		assert(peak_load_next(pcap) == test_pcap_len[i]);
-		assert(be16dec(&eth->ether_type) == ETHERTYPE_IP);
-	}
-
-	assert(!peak_load_next(pcap));
-	assert(!peak_load_next(pcap));
-
-	peak_load_exit(pcap);
-}
-
-static const unsigned int test_pcapng_len[] = {
+static const unsigned int pcapng_len[] = {
 	78,
 };
 
-static void
-test_pcapng(void)
-{
-	struct peak_load *pcap = peak_load_init("../../sample/test.pcapng");
-	struct ether_header *eth;
-	unsigned int i;
+static const char *pcapng_file = "../../sample/test.pcapng";
 
-	assert(pcap);
-
-	eth = (struct ether_header *) pcap->buf;
-
-	for (i = 0; i < lengthof(test_pcapng_len); ++i) {
-		assert(peak_load_next(pcap) == test_pcapng_len[i]);
-		assert(be16dec(&eth->ether_type) == ETHERTYPE_IP);
-	}
-
-	assert(!peak_load_next(pcap));
-	assert(!peak_load_next(pcap));
-
-	peak_load_exit(pcap);
-}
-
-static const unsigned int test_erf_len[] = {
+static const unsigned int erf_len[] = {
 	78, 64, 64, 711, 64, 1470, 64, 1470, 64, 393,
 	64, 711, 64, 1470, 262, 64, 64, 64, 64,
 };
 
+static const char *erf_file = "../../sample/test.erf";
+
 static void
-test_erf(void)
+test_load(const char *file, const unsigned int *len, const size_t count)
 {
-	struct peak_load *erf = peak_load_init("../../sample/test.erf");
+	struct peak_load *trace = peak_load_init(file);
 	struct ether_header *eth;
-	unsigned int i;
+	size_t i;
 
-	assert(erf);
+	assert(trace);
 
-	eth = (struct ether_header *) erf->buf;
+	eth = (struct ether_header *)trace->buf;
 
-	for (i = 0; i < lengthof(test_erf_len); ++i) {
-		assert(peak_load_next(erf) == test_erf_len[i]);
+	for (i = 0; i < count; ++i) {
+		assert(peak_load_next(trace) == len[i]);
 		assert(be16dec(&eth->ether_type) == ETHERTYPE_IP);
 	}
 
-	assert(!peak_load_next(erf));
-	assert(!peak_load_next(erf));
+	assert(!peak_load_next(trace));
+	assert(!peak_load_next(trace));
 
-	peak_load_exit(erf);
+	peak_load_exit(trace);
 }
 
 int
@@ -117,9 +79,9 @@ main(void)
 {
 	pout("peak load test suite... ");
 
-	test_pcap();
-	test_pcapng();
-	test_erf();
+	test_load(pcap_file, pcap_len, lengthof(pcap_len));
+	test_load(pcapng_file, pcapng_len, lengthof(pcapng_len));
+	test_load(erf_file, erf_len, lengthof(erf_len));
 
 	pout("ok\n");
 
