fa6cedcd5357c4b90cd41bb286812bff7328bd11
Franco Fichtner <franco@packetwerk.com>
2013-12-10 23:30:58 +0100

    regress: migrate peek(1) tests to new format

--- regress/peek/amend.sh
+++ regress/peek/amend.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
-then
-	echo "usage: $0 test_trace"
+if [ $# -eq 0 ]; then
+	echo "usage: $0 test ..."
 	exit
 fi
 
-../../bin/peek/peek $1.in > $1.out
+for TEST in "$@"; do
+	[ "$TEST" = "amend.sh" -o "$TEST" = "Makefile" ] && continue
+
+	LINES=`grep "$TEST" Makefile`
+
+	[ -z "$LINES" ] && echo "unknown $TEST" && continue
+
+	set `echo "$LINES"`
+	while : ; do
+		case $# in
+		[012]) echo "ambigous $TEST" && break;;
+		esac;
+		if [ ! "$1" = "$TEST" -o ! "$3" = "\\" ]; then
+			shift; shift; shift
+			continue
+		fi
+		../../bin/peek/peek ../../sample/$2 \
+		    > $1 && echo "rewrote $1"
+		break;
+	done
+done
